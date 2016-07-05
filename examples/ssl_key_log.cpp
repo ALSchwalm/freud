657b5b8fe5cd0caf53c03facdec1c5508a6907ac
Adam Schwalm <adamschwalm@gmail.com>
2016-07-05 14:56:30 -0500

    More complete ssl key log example

--- examples/ssl_key_log.cpp
+++ examples/ssl_key_log.cpp
@@ -1,6 +1,8 @@
 #include "freud/freud.hpp"
 #include <iostream>
 #include <openssl/ssl.h>
+#include <set>
+#include <unistd.h>
 
 class SSL_SESSION_Matcher : public freud::MemoryObject<SSL_SESSION> {
 public:
@@ -8,22 +10,32 @@ public:
                        freud::address_t address) {
         bool res = e.ssl_version == 0x303 && e.master_key_length == 48 &&
                    e.session_id_length == 32;
-        if (res)
-            std::cout << "Found match at " << address << std::endl;
-
         return res;
     }
+
+    static void before_check() { usleep(1); }
 };
 
 int main(int argc, char** argv) {
+    std::ofstream out("keys.log");
+    std::set<freud::address_t> known_matches;
+
     freud::MemoryContext ctx(atoi(argv[1]), true);
 
     freud::MemoryContextIterator<SSL_SESSION_Matcher> iter =
-        ctx.scan_once<SSL_SESSION_Matcher>();
+        ctx.scan_forever<SSL_SESSION_Matcher>();
 
-    for (; iter != ctx.end(); ++iter) {
-        std::cout << "RSA Session-ID:" << freud::format_as_hex(iter->session_id)
-                  << " Master-Key:" << freud::format_as_hex(iter->master_key)
-                  << std::endl;
+    for (;; ++iter) {
+        if (!known_matches.count(iter.address())) {
+            std::cout << "RSA Session-ID:"
+                      << freud::format_as_hex(iter->session_id)
+                      << " Master-Key:"
+                      << freud::format_as_hex(iter->master_key) << std::endl;
+
+            out << "RSA Session-ID:" << freud::format_as_hex(iter->session_id)
+                << " Master-Key:" << freud::format_as_hex(iter->master_key)
+                << "\n";
+            known_matches.insert(iter.address());
+        }
     }
 }
