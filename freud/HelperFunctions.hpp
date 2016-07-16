cd10f57b7c9e49cd2d370ed8c0286777b30b1c73
Adam Schwalm <adamschwalm@gmail.com>
2016-07-15 19:27:04 -0500

    Add is_valid_address helper

--- freud/HelperFunctions.hpp
+++ freud/HelperFunctions.hpp
@@ -1,6 +1,7 @@
 #ifndef FREUD_HELPER_FUNCTIONS
 #define FREUD_HELPER_FUNCTIONS
 
+#include "freud/MemoryContext.hpp"
 #include <cstdio>
 #include <iomanip>
 #include <iostream>
@@ -33,10 +34,29 @@ std::string format_as_hex(Iter begin, Iter end) {
     return ss.str();
 }
 
-/// Format the bytes in the given array into a string of hex characters
+/** \brief Format the bytes in the given array into a string of hex characters
+ *
+ * See the documentation for the iterator-based `format_as_hex`.
+ */
 template <typename T, std::size_t Size>
 std::string format_as_hex(const T (&arr)[Size]) {
     return format_as_hex(&arr[0], &arr[0] + Size);
+}
+
+/** \brief Determine if a give address is valid within a context
+ *
+ * If the user is verifying a structure with some pointer fields,
+ * it is often sufficient to test if the pointer value exists in
+ * a mapped region of memory, as this is unlikely to occur by
+ * coincidence. This function provides a convenient way to perform
+ * this test.
+ */
+template <typename T>
+bool is_valid_address(const MemoryContext& ctx, const T* address) {
+    const std::vector<MemoryRegion>& regions = ctx.mapped_regions();
+    typename std::vector<MemoryRegion>::const_iterator iter =
+        ctx.region_containing(reinterpret_cast<address_t>(address));
+    return iter != regions.end();
 }
 }
 
