0018fbed69fc34f1e85a7121847e3b90644cfd10
Adam Schwalm <adamschwalm@gmail.com>
2016-07-17 14:45:16 -0500

    Add description to example

--- examples/ssl_key_log.cpp
+++ examples/ssl_key_log.cpp
@@ -1,3 +1,24 @@
+/** ssl_key_log.cpp
+ *
+ * This file is a brief example that will capture and
+ * record SSL session master secrets from an application
+ * using OpenSSL sessions.
+ *
+ * Compile this program with 'g++ -I. examples/ssl_key_log.cpp -o key_log'
+ *
+ * The resulting 'key_log' binary can be run (as root) with:
+ * './key_log <some pid>'. Where <some pid> is the PID of the
+ * target process.
+ *
+ * While running, this program will log keys to 'keys.log' and
+ * to standard output. After you have collected the keys,
+ * you can decrypt some wireshark traffic by selecting:
+ *
+ * Edit->Preferences->Protocols->SSL
+ *
+ * Add adding the 'keys.log' file as the '(Pre)-Master-Secret Log Filename'
+ */
+
 #include "freud/freud.hpp"
 #include <iostream>
 #include <openssl/ssl.h>
@@ -12,15 +33,13 @@ public:
                    e.session_id_length == 32;
         return res;
     }
-
-    static void before_check() { usleep(1); }
 };
 
 int main(int argc, char** argv) {
     std::ofstream out("keys.log");
     std::set<freud::address_t> known_matches;
 
-    freud::MemoryContext ctx(atoi(argv[1]), true);
+    freud::MemoryContext ctx(atoi(argv[1]));
 
     freud::MemoryContextIterator<SSL_SESSION_Matcher> iter =
         ctx.scan_forever<SSL_SESSION_Matcher>();
